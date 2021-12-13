8bdccf00d241ae8e9649957d61c93b26350e43a0
Joel Lord <joelphy@gmail.com>
2021-12-13 15:03:26 -0500

    Cleanup after authentication changes

--- front/src/index.js
+++ front/src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { Auth0Provider } from "@auth0/auth0-react";
-
-// const domain = "conf-tracker.auth0.com";
-// const clientId = "1pCOZe2pocuG3Roz8O0ujg5JIV5Dk90X";
 
 ReactDOM.render(
-  // <Auth0Provider
-  //     domain={domain}
-  //     clientId={clientId}
-  //     audience={"https://conftracker.com"}
-  //     redirectUri={window.location.origin}
-  //   >
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-  // </Auth0Provider>,
   document.getElementById('root')
 );
 
